4e1f37defcef00123809364c6ed380692cfc441d
Alonso Alejandro Alarcón Parra <155941412+alxxjandro@users.noreply.github.com>
2025-04-01 19:09:20 +0000

    agregue una router para obtener un departamento por ID

--- routes/departamento.js
+++ routes/departamento.js
@@ -14,7 +14,41 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  let transaction;
+  try {
+    await sql.connect(DBconfig);
+    const idQuery = parseInt(req.params.id);
+    console.log(idQuery);
+
+    if (!idQuery) {
+      res.send("ID NO VALIDO");
+    } else {
+      transaction = new sql.Transaction();
+      await transaction.begin();
+      const request = new sql.Request(transaction);
+      request.input("searchID", sql.Int, idQuery);
+
+      const result = await request.query(
+        "SELECT * FROM departamento WHERE idDepartamento = @searchID",
+      );
+      if (result.recordset.length) {
+        res.send(result.recordset);
+      } else {
+        res.send("EL ID NO EXISTE");
+      }
+      await transaction.commit();
+    }
+  } catch (error) {
+    console.error("No se ejecuto la transaccion:", error);
+    await transaction.rollback();
+  } finally {
+    sql.close();
+  }
+});
+
 router.post("/", async (req, res) => {
+  let transaction;
   try {
     await sql.connect(DBconfig);
     const dpto = req.body;
@@ -22,7 +56,7 @@ router.post("/", async (req, res) => {
       `Agregando el departamento: ${dpto.nombreDepartamento} con ID: ${dpto.idDepartamento}`,
     );
 
-    const transaction = new sql.Transaction();
+    transaction = new sql.Transaction();
     await transaction.begin();
 
     const request = transaction.request();
